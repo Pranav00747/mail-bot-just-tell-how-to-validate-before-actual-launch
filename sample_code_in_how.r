fba26d771c2802d70fd0f4c8596ad0124a932854
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2022-12-26 05:33:58 +0530

    Create sample_code_in_how.r

---
+++ sample_code_in_how.r
@@ -0,0 +1,24 @@
+get_msgs <- function(host, email, pass)
+{
+    cc <- imap_connect(host, email, pass, 993)
+    folder <- cc.get_folder("[Gmail]/All Mails")
+    folder.open(READ_ONLY)
+    return folder.get_messages()
+ }
+ 
+ msgs <- get_msgs("imap.google.com", "mail@gmail.com", "password123")
+ sub <- msgs[0].subject
+ frm <- msgs[0].from
+ to <- msgs[0].reciept_to
+ text <- msgs[0].content
+ 
+ send_msg<-function(from, to, subject, text)
+ {
+ smtp_send<- smtp_connect(host, email, pass, 587)
+ smtp_send<- send_mail(from, subject, to, text)
+ }
+ 
+ 
+ 
+     
+ 
