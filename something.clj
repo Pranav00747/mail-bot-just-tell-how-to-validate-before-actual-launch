e71a405af89f74257c3115770e7fdd5120a88fca
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2023-04-01 20:49:23 +0530

    Create something.clj

---
+++ something.clj
@@ -0,0 +1,6 @@
+(defn just_tell_you_one_thing[this_or_past](
+  if(this_or_past)
+     (def if_me "This had been created in fiftheen to thirty minutes and i have shared only just of part of codes on github, our loyaly truely exist.")
+     ))
+     
+     
