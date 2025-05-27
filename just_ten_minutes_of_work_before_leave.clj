da8fc3762bf3ba43ca02905d13af9692e2e60db1
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2025-05-27 20:14:54 +0530

    Delete just_ten_minutes_of_work_before_leave.clj

--- just_ten_minutes_of_work_before_leave.clj
+++
@@ -1,5 +0,0 @@
-(defn something_i_wanna_tell[](
-     ;; Just telling you how to validate email before actual launch and
-     ;; Soon i will launch this Mail API also Deep Learning Kit and Chat GPT updating that is another thing 
-     ;; Just a ten minutes of work before leaving guys because i am not that place where i want to be, lots of love
-))
